39085c74caee7dbd3ae10908ed46bebcb37dea4c
Jonhscz <jonhsmith.cordova@gmail.com>
2021-07-13 14:06:52 -0500

    DOCS: Added technology icons

--- src/pages/components/Social/Social.js
+++ src/pages/components/Social/Social.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+import Html from "../svg/Html";
+import Css from "../svg/Css";
+import JavaScript from "../svg/JavaScript";
+import Angular from "../svg/Angular";
+import Bootstrap from "../svg/Bootstrap";
+import CodeSandbox from "../svg/CodeSandbox";
+import Cplusplus from "../svg/Cplusplus";
+import DigitalOcean from "../svg/DigitalOcean";
+import EclipseIde from "../svg/EclipseIde";
+import Figma from "../svg/Figma";
+import ReactSvg from "../svg/ReactSvg";
+import Java from "../svg/Java";
+import Spring from "../svg/Spring";
+import SpringBoot from "../svg/SpringBoot";
+
 import "./social.scss";
 
 const Social = () => {
@@ -8,6 +23,20 @@ const Social = () => {
       <li>Git Hub</li>
       <li>LinkedIn</li>
       <li>Email</li>
+      <Html />
+      <Css />
+      <JavaScript />
+      <Angular />
+      <Bootstrap />
+      <CodeSandbox />
+      <Cplusplus />
+      <DigitalOcean />
+      <EclipseIde />
+      <Figma />
+      <ReactSvg />
+      <Java />
+      <Spring />
+      <SpringBoot />
     </ul>
   );
 };
